b4b142156bba3e2bcd34dd0699412cfd2dfe0e19
Elber-Kevenny <vieirabeb@gmail.com>
2025-09-17 16:26:30 -0300

    add new solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,37 @@
 'use strict';
-
 /**
  * @param {Object} state
  * @param {Object[]} actions
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const contagem = [{ ...state }];
+
+  for (const n of actions) {
+    const lastState = contagem[contagem.length - 1];
+    let newState = { ...lastState };
+
+    switch (n.type) {
+      case 'addProperties':
+        Object.assign(newState, n.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of n.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+
+      case 'clear':
+        newState = {};
+        break;
+    }
+    contagem.push(newState);
+  }
+
+  return contagem;
 }
 
 module.exports = transformStateWithClones;
